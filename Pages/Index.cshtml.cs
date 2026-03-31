e9400777ecc2a1e7873022b94992a35bc03c75c4
carlosholimpio777-debug <carlosholimpio777.com>
2026-03-31 11:25:39 -0300

    estrutura base

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace acessoriostecnologicos.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
